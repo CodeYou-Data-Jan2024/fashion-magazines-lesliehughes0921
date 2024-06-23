298fe0e3b037ddaf4ddb5dc3fa96f36d0950e420
lesliehughes0921 <leslie.hughes0921.com>
2024-06-23 14:41:40 -0400

    Added SQL/Fashion_Magazines file

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,15 @@
--- Add your sql here
+SELECT
+    customers.customer_name AS Customer,
+    PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS "Amount Due"
+FROM
+    customers
+JOIN
+    orders ON customers.customer_id = orders.customer_id
+JOIN
+    subscriptions ON orders.subscription_id = subscriptions.subscription_id
+WHERE
+    orders.order_status = 'unpaid'
+    AND subscriptions.description = 'Fashion Magazine'
+GROUP BY
+    customers.customer_name;
+
